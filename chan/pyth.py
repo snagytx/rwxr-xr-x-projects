53883e83d5e3a9f0aba1902bcd5f211d7ec1cf16
ngsebastian@gmail.com <ngsebastian@gmail.com@fc0b48f5-12a1-ce7c-3d9c-f56c987e7e85>
2012-02-03 22:21:28 +0000

    123

--- chan/pyth.py
+++ chan/pyth.py
@@ -1,5 +1,5 @@
 addLink('Cinemax', 'http://www.lyngsat-logo.com/logo/tv/cc/cinemax_hdtv.jpg', 'rtmp://s99.webport.tv/live/ playpath=z050221.stream swfUrl=http://www.tvsector.com/wp-content/uploads/jw-player-plugin-for-wordpress/player/player.swf  pageUrl=http://www.tvsector.com/national-geographic-nat-geo-hd/')
 addLink('Cinemax 2', 'http://www.lyngsat-logo.com/logo/tv/cc/cinemax2_ce.jpg', 'rtmp://s99.webport.tv/live/ playpath=z010401.stream swfUrl=http://www.tvsector.com/wp-content/uploads/jw-player-plugin-for-wordpress/player/player.swf pageUrl=http://www.tvsector.com/national-geographic-nat-geo-hd/')
 addLink('Food Network', 'http://www.lyngsat-logo.com/logo/tv/ff/food_network_us_hd.jpg', 'rtmp://176.31.231.57/edge/ playpath=7pb51t4nqxrmubw swfUrl=http://static.ilive.to/jwplayer/player.swf pageUrl=http://www.ilive.to/view/23694')
-addLink('Food Network', 'http://www.lyngsat-logo.com/logo/tv/ff/food_network_us_hd.jpg', 'rtmp://176.31.231.57/edge/  playpath=7pb51t4nqxrmubw  swfUrl=http://static.ilive.to/jwplayer/player.swf  pageUrl=http://www.ilive.to/view/23694')
-
+addLink('Starz', 'http://www.lyngsat-logo.com/logo/tv/ss/starz.jpg', 'rtmp://38.96.175.173:1935/live/ playpath=20012@hyqw  swfUrl=http://www.tvopencast.com/tvopencast.com.swf  pageUrl=http://tvopencast.com/video.php?n=20012@hyqw')
+addLink('HGTV', 'http://www.lyngsat-logo.com/hires/hh/hgtv.png', 'rtmp://fms3.tikilive.com:1935/view/25503_122/stream25503 live=true timeout=60')
