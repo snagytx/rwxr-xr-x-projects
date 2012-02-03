44dc2f1ba2a247141afedbaf7042ca1531202fdc
ngsebastian@gmail.com <ngsebastian@gmail.com@fc0b48f5-12a1-ce7c-3d9c-f56c987e7e85>
2012-02-03 22:08:51 +0000

    asdc

---
+++ chan/pyth.py
@@ -0,0 +1,4 @@
+addLink('Cinemax', '', 'rtmp://s99.webport.tv/live/ playpath=z050221.stream swfUrl=http://www.tvsector.com/wp-content/uploads/jw-player-plugin-for-wordpress/player/player.swf  pageUrl=http://www.tvsector.com/national-geographic-nat-geo-hd/')
+addLink('Cinemax 2', '', 'rtmp://s99.webport.tv/live/ playpath=z010401.stream swfUrl=http://www.tvsector.com/wp-content/uploads/jw-player-plugin-for-wordpress/player/player.swf pageUrl=http://www.tvsector.com/national-geographic-nat-geo-hd/')
+addLink('Food Network', '', 'rtmp://176.31.231.57/edge/ playpath=7pb51t4nqxrmubw swfUrl=http://static.ilive.to/jwplayer/player.swf pageUrl=http://www.ilive.to/view/23694')
+
